b4e6040130014939db756271e3f6c78a6bcf1cfe
maxvone <maximvone@gmail.com>
2023-09-29 20:51:07 +0200

    add: add open neogit mapping

--- mappings.lua
+++ mappings.lua
@@ -30,6 +30,7 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
+    ["<leader>gn"] = { ":Neogit<cr>" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
